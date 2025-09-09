3a3fca2436cb12bb10f2f4d9bef2e7db43d2bd3e
sachin sharma <harshit0808sharma@gmail.com>
2025-09-09 13:18:29 +0530

    some other changes

--- app/api/salons/[slug].js/route.js
+++ app/api/salons/[slug].js/route.js
@@ -1,9 +1,9 @@
-// app/api/salons/[slug]/route.js
-import { getSalonBySlug } from "../../../../lib/salons";
+// // app/api/salons/[slug]/route.js
+// import { getSalonBySlug } from "../../../../lib/salons";
 
-export async function GET(request, { params }) {
-  const { slug } = params;
-  const salon = await getSalonBySlug(slug);
-  if (!salon) return new Response(JSON.stringify({ message: "Not found" }), { status: 404, headers: { "Content-Type": "application/json" } });
-  return new Response(JSON.stringify(salon), { headers: { "Content-Type": "application/json" } });
-}
+// export async function GET(request, { params }) {
+//   const { slug } = params;
+//   const salon = await getSalonBySlug(slug);
+//   if (!salon) return new Response(JSON.stringify({ message: "Not found" }), { status: 404, headers: { "Content-Type": "application/json" } });
+//   return new Response(JSON.stringify(salon), { headers: { "Content-Type": "application/json" } });
+// }
